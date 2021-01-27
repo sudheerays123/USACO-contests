c889bb8670da0c6349eaddd96cd8014ad1510377
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-01-27 14:22:17 +0530

    Update SquarePasture.cpp

--- 2016-2017/2016 December/bronze/SquarePasture.cpp
+++ 2016-2017/2016 December/bronze/SquarePasture.cpp
@@ -1,3 +1,5 @@
+// Method 1 :
+
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false); cin.tiille(NULL); cout.tie(NULL);
@@ -40,3 +42,60 @@ int main(){
 	
 	cout out side*side;
 }
+
+// Method 2 : ( Using Struct ) --> 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define tc ll test;cin >> test;while(test--)
+#define vi vector<ll>
+#define pb push_back
+#define mp make_pair
+#define INF 0x3f3f3f3f3f
+#define MOD 1e9 + 7
+#define ff first
+#define ss second
+#define in >>
+#define out <<
+#define space << " " <<
+#define spacef << " "
+#define fo(i,a,b) for(ll i = a; i <= b; i++)
+#define nextline out "\n"
+#define print(x) for(auto i : x ) cout out i.first space i.second nextline
+#define mmax(x,i) x = max(x,i)
+#define mmin(x,i) x = min(x,i)
+
+struct Rect{
+    int x1,y1,x2,y2;
+};
+
+int main() {
+
+    freopen("square.in","r",stdin);
+    freopen("square.out","w",stdout);
+
+    Rect first,second;
+
+    cin in first.x1 in first.y1 in first.x2 in first.y2;
+    cin in second.x1 in second.y1 in second.x2 in second.y2;
+
+    ll lowesty = min(first.y1 , second.y1);
+    ll highesty = max(first.y2 , second.y2);
+
+    ll lowestx = min(first.x1 , second.x1);
+    ll highestx = max(first.x2 , second.x2);
+
+    ll sidev = highesty - lowesty;
+    ll sideh = highestx - lowestx;
+
+    ll minside = max(sidev , sideh);
+
+    cout out minside * minside;
+
+    return 0;
+}
+
