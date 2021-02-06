0867507bd28a31040761f704ba690cf91c1ca43c
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-02-06 18:27:06 +0530

    Create Dance Show.cpp

---
+++ Dance Show.cpp
@@ -0,0 +1,48 @@
+http://www.usaco.org/index.php?page=viewproblem2&cpid=690
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long int ll;
+#define tc ll test;cin >> test;while(test--)
+#define vi vector<ll>
+#define pb push_back
+#define mp make_pair
+#define INF 0x3f3f3f3f3f
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define in >>
+#define out <<
+#define space << " " <<
+#define spacef << " "
+#define fo(i,a,b) for(ll i = a; i <= b; i++)
+#define nextline out "\n"
+#define print(x) for(auto i : x ) cout out i spacef
+#define mmax(x,i) x = max(x,i)
+#define mmin(x,i) x = min(x,i)
+
+bool isKpossible(ll k){
+
+}
+
+int main() {
+
+    ll n,t;
+    cin in n in t;
+
+    vi d(n+5);
+    fo(i,1,n) cin in d[i];
+
+    ll low = 1;
+    ll high = n;
+
+    while(low <= high){
+
+    }
+
+    return 0;
+}
